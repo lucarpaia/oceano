b2d1a88ce0de245ffb637a1dfe77b826514217e7
GiuseppeOrlando878776 <giuseppe1.orlando@mail.polimi.it>
2024-07-25 16:41:51 +0200

    Added auxiliary structs for the mesh refinement

--- refine_mesh.h
+++ refine_mesh.h
@@ -1,6 +1,30 @@
 #include <deal.II/grid/grid_refinement.h>
 #include <deal.II/distributed/solution_transfer.h>
 
+// Auxiliary structs for mesh adaptation procedure
+//
+template<int dim>
+struct ScratchData {
+  ScratchData(const FiniteElement<dim>& fe,
+              const unsigned int        quadrature_degree,
+              const UpdateFlags         update_flags): fe_values(fe, QGauss<dim>(quadrature_degree), update_flags) {}
+
+  ScratchData(const ScratchData<dim>& scratch_data): fe_values(scratch_data.fe_values.get_fe(),
+                                                               scratch_data.fe_values.get_quadrature(),
+                                                               scratch_data.fe_values.get_update_flags()) {}
+  FEValues<dim> fe_values;
+};
+
+
+struct CopyData {
+  CopyData() : cell_index(numbers::invalid_unsigned_int), value(0.0)  {}
+
+  CopyData(const CopyData &) = default;
+
+  unsigned int cell_index;
+  double       value;
+};
+
 // @sect{ <code>NavierStokesProjection::refine_mesh</code>}
 
 // After finding a good initial guess on the coarse mesh, we hope to
